f160b5c588628b3bd8ad715461d15958d859f9df
unknown <1743769392@qq.com>
2024-05-28 00:24:38 +0800

    change2

--- src/test/java/com/example/demo001/Demo001ApplicationTests.java
+++ src/test/java/com/example/demo001/Demo001ApplicationTests.java
@@ -1,13 +1,56 @@
 package com.example.demo001;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import java.io.File;
 
 @SpringBootTest
 class Demo001ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private JavaMailSender sender;          //邮件发送器
+	private String from = "1743769392@qq.com";    //发件人
+	private String to = "jerry_test@sohu.com";      //收件人
+	private String subject = "This is a test Mail"; //标题
+	private String context = "Hello,Java Mail"; //正文
+	
+
+	@Test
+	public void testSendSimpleMail() {
+		SimpleMailMessage mail = new SimpleMailMessage();   //简单邮件
+		mail.setSubject(subject);
+		mail.setText(context);
+		mail.setFrom(from);
+		mail.setTo(to);
+		sender.send(mail);  //发送邮件
+		System.out.println("邮件发送成功！");
+	}
+
+	@Test
+	public void testSendMimeMail() throws MessagingException {
+		MimeMessage mail = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mail,true);
+		helper.setSubject(subject);
+		helper.setText(context+"<img src=''/>",true);
+		helper.setFrom(from);
+		helper.setTo(to);
+		File file = new File("");
+		helper.addAttachment(file.getName(),file);
+		sender.send(mail);
+		System.out.println("邮件发送成功！");
+
 	}
 
 }
